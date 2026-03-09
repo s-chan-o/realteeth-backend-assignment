b6b5b6cd42ddf171cf6f1adac7d0f42c33aaf703
seungchan <s24041@gsm.hs.kr>
2026-03-09 23:42:53 +0900

    chore: initialize project

--- src/main/kotlin/com/seungchan/realteeth/RealteethTaskApplication.kt
+++ src/main/kotlin/com/seungchan/realteeth/RealteethTaskApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class RealteethTaskApplication
 
 fun main(args: Array<String>) {
-	runApplication<RealteethTaskApplication>(*args)
+    runApplication<RealteethTaskApplication>(*args)
 }
